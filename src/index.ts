65ef675ba57e37798716b5885e559558378ba821
Gabriele Lo Re <gabrolr70@gmail.com>
2024-07-18 15:44:51 +0200

    feat(chat): first version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,61 @@
-import express from "express";
+// #region ::: IMPORT :::
+import express, { Request, Response } from "express";
+import { createClient } from "@vercel/postgres";
+import { config } from "dotenv";
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+
+// #endregion
+
+// #region ::: CONFIGURATION :::
+config();
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const client = createClient({ connectionString: process.env.DATABASE_URL });
+
+app.use(express.json());
+// #endregion
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+io.on("disconnect", (socket) => {
+  console.log("A user disconnected");
+});
+
+client.connect();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/messages", (req: Request, res: Response) => {
+  client.query("SELECT * FROM messages", (error, response) => {
+    if (error) res.status(500).json({ error });
+    else res.status(200).json(response.rows);
+  });
+});
+
+app.post("/api/messages", (req: Request, res: Response) => {
+  const { content } = req.body;
+  client.query(
+    `INSERT INTO messages (content) VALUES ($1)`,
+    [content],
+    (error) => {
+      if (error) res.status(500).json({ error });
+      else res.status(200).json({ message: "Message created successfully" });
+    }
+  );
+});
+
+server.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
+});
+
+app.listen(8080, () => {
+  console.log(`Server running on http://localhost:8080`);
+});
